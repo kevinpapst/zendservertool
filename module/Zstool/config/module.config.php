c2111ad7919904f41a7cbb3d56facff04c94a901
kevinpapst <kpapst@gmx.net>
2012-12-21 16:42:00 +0100

    intial commit

--- module/Zstool/config/module.config.php
+++ module/Zstool/config/module.config.php
@@ -78,7 +78,7 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'zstool/index/index'      => __DIR__ . '/../view/zstool/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
